3823e6b98d3c85dc615080e65f9e1285e9c09189
Kellyn Flanagan <kflanagan@lat-kflanagan1.local>
2015-02-02 18:08:35 -0500

    added contact page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,23 @@
 class WelcomeController < ApplicationController
+
   def index
   end
 
   def about
   end
+
+    def contact
+    end
+
+    private
+        def set_color
+
+         @name = params[:name]
+            #if there is a name provided, set it for the session
+            if @name
+                 session[:name] = @name
+            else
+                @name = session[:name]
+            end
+    end
 end
